879fb8565e70d3273d5077c35cbb53e8128bee9b
soul <51918809+soul-science@users.noreply.github.com>
2021-03-30 16:35:45 +0800

    Add files via upload

--- pso.py
+++ pso.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class Pso(object):
@@ -28,6 +29,7 @@ class Pso(object):
                     self.al_best_x[i] = self.al_x[i]
 
             dm = np.argmin(self.al_best_e)
+            # print(self.al_best_x, self.al_best_e)
             if self.best_e > self.al_best_e[dm]:
                 self.best_e = self.al_best_e[dm]
                 self.best_x = self.al_best_x[dm]
@@ -51,7 +53,8 @@ class Pso(object):
             self.al_x[i][self.al_x[i] < self.extent[0]] = self.extent[0]
             self.al_x[i][self.al_x[i] > self.extent[1]] = self.extent[1]
 
-    def fit(self, for_=10000):
+    def fit(self, for_=1000):
+        self.for_ = for_
         self.w = lambda w: (1 + 1 / for_ * 2) ** (-w)
 
         for i in range(for_):
@@ -60,6 +63,14 @@ class Pso(object):
 
     def result(self):
         return [self.best_x, self.best_e]
+
+    def draw_w(self):
+        if self.w is None:
+            raise ValueError("w is None, you should fit firstly!")
+        x = np.linspace(0, self.for_, self.for_*10)
+        y = self.w(x)
+        plt.plot(x, y)
+        plt.show()
 
 
 def draw(f, extent, point, interval):
@@ -70,15 +81,31 @@ def draw(f, extent, point, interval):
     plt.show()
 
 
+def draw_3d(f, extent, point, interval):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    x = np.linspace(extent[0], extent[1], interval)
+    y = np.linspace(extent[0], extent[1], interval)
+    # x.shape = (1, x.shape[0])
+    # y.shape = (1, y.shape[0])
+    x, y = np.meshgrid(x, y)
+    print(x.shape, y.shape, f(x, y).shape)
+    ax.plot_surface(x, y, f(x, y), rstride=1, cstride=1)
+    ax.scatter(point[0][0], point[0][1], point[1], c='r', marker='^')
+    ax.view_init(elev=30, azim=125)
+    plt.show()
+
+
 if __name__ == '__main__':
     import time
-    f1 = lambda x: x * np.sin(x) + x * np.cos(2*x)
-    # f1 = lambda x: x * np.sin(x)**2 - x * np.cos(x)**2
-    # f1 = lambda x: np.log(x + 1) - x * np.cos(x)
-    pso = Pso(f1, 1, [0, 10], al=10)
     t = time.time()
-    pso.fit(10)
+    # f1 = lambda x: x * np.sin(x) + x*np.cos(2*x)
+    f1 = lambda x: np.sum(x**2/4000) - np.cos(x/np.sqrt(np.arange(1, x.shape[0]+1))).prod() + 1
+    f2 = lambda x, y: (x**2 + y**2)/4000 - np.cos(x/np.sqrt(1)) * np.cos(y/np.sqrt(2)) + 1
+    pso = Pso(f1, 2, [-8, 8])
+    pso.fit(100)
     print(time.time() - t)
     x, y = pso.result()
     print(x, y)
-    draw(f1, [0, 10], [x, y], 1000)
+    # draw(f1, [-10, 10], [x, y], 1000)
+    draw_3d(f2, [-8, 8], [x, y], 160)
